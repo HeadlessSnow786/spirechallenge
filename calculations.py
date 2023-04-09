f55219f1f7050ebd525bbf96fa410b1a5029ed4b
ArS377 <arysom992@gmail.com>
2023-04-09 03:03:45 -0700

    finshed making all four thresholds

--- calculations.py
+++ calculations.py
@@ -12,9 +12,7 @@ payload = json.dumps(
             "waypoints": [
                 {"time": "2023-04-8T12:00:00", "lat": 40.526101, "lon": -74.039306},
                 {"time": "2023-04-8T13:00:00", "lat": 39.97322, "lon": -71.954218},
-                {"time": "2023-04-8T14:00:00", "lat": 39.485893, "lon": -66.749471},
-                {"time": "2023-04-8T15:00:00", "lat": 37.434774, "lon": -53.753772},
-                {"time": "2023-04-8T16:00:00", "lat": 36.693392, "lon": -42.424287},
+                {"time": "2023-04-8T14:00:00", "lat": 40.2496605, "lon": -72.996762},
             ],
         },
         "bundles": "basic,maritime",
@@ -24,7 +22,7 @@ url = "https://api.wx.spire.com/forecast/route"
 response = requests.request("POST", url, headers=headers, data=payload)
 
 json_data = response.json()
-print(type(json_data))
+#print(type(json_data))
 
 
 """
@@ -33,16 +31,15 @@ hour2 = int(payload["route"]["waypoints"][1]["time"][10:12])
 """
 
 #getting the "lat": 40.526101, "lon": -74.039306 and "lat": 39.97322, "lon": -71.954218
-print(payload)
+#print(payload)
 x_coor1 = float(json_data["data"][0]["location"]["coordinates"]["lat"])
 y_coor1 = float(json_data["data"][0]["location"]["coordinates"]["lon"])
 x_coor2 = float(json_data["data"][1]["location"]["coordinates"]["lat"])
 y_coor2 = float(json_data["data"][1]["location"]["coordinates"]["lon"])
-
-
-x_mid = (x_coor2 - x_coor1)/2
-y_mid = (y_coor2 - y_coor1)/2
-
+x_mid = float(json_data["data"][2]["location"]["coordinates"]["lat"])
+y_mid = float(json_data["data"][2]["location"]["coordinates"]["lon"])
+x_dif = x_coor2 - x_coor1
+y_dif = y_coor2 - y_coor1
 """
 print(x_coor1)
 print(x_coor2)
@@ -54,25 +51,44 @@ print(rise)
 print(run)
 """
 
-
-while True: #keep incrementing coordinates until reached the end
+reason_list = []
+danger = False
+danger_cood = []
+reason = "Reason(s) for danger: "
+for x in range(3): #keep incrementing coordinates until reached the end
+    #print(x_coor1)
+    #print(y_coor1)
+    wind_speed = float(json_data["data"][x]["values"]["wind_speed"])
+    precip_rate = float(json_data["data"][x]["values"]["precipitation_rate"])
+    visibility = float(json_data["data"][x]["values"]["surface_visibility"])
+    wave_height = float(json_data["data"][x]["values"]["sea_surface_wave_significant_height"])
+    if wind_speed > 8.9: #8.9 m/s is fast winds
+        danger = True
+        danger_cood.append((x_coor1, y_coor1))
+        reason = reason + "Wind Speed is too high. "
+    elif precip_rate > 0.0035: #0.5 inch/hr is considered high precipitation rate
+        danger = True
+        danger_cood.append((x_coor1, y_coor1))
+        reason = reason + "Precipiation rate is too high. "
+    elif visibility < 3704: #2 nautical miles or less considered poor visibility
+        danger = True
+        danger_cood.append((x_coor1, y_coor1))
+        reason = reason + "Surface Visibility is too low. "
+    elif wave_height > 5:
+        danger = True
+        danger_cood.append((x_coor1, y_coor1))
+        reason = reason + "Waves are too high."
+    x_coor1 += x_dif
+    y_coor1 += y_dif 
+    reason_list.append(reason)
     print(x_coor1)
     print(y_coor1)
-    #printing each increment
-    x_coor1 += x_mid
-    y_coor1 += y_mid #incrementing
-    #check the coordinate if it goes above for any of the conditions
 
-    if x_coor1 >= x_coor2 and x_mid > 0: #break if start is greater than end if run is increases
-        break
-    elif x_coor1 <= x_coor2 and x_mid < 0: #break if start is less than end if run decreases
-        break
+if reason_list[0] == "Reason(s) for danger: ":
+    print("This route looks safe. ")
+else:
+    print(set(danger_cood))
+    print(reason_list)
 
-print(x_coor1)
-print(y_coor1)
-
-
-
-    
 
 
