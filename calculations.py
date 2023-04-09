82b8c81964b44812cee23162a541e6c2678a0bc9
ArS377 <arysom992@gmail.com>
2023-04-08 22:47:10 -0700

    many modifications

--- calculations.py
+++ calculations.py
@@ -1,33 +1,47 @@
 import requests
 import json
 
-url = "https://api.wx.spire.com/forecast/route"
-
-
-points = {
-        "route": {"name": "my_example_route",
-        "waypoints": [
-            {"time": "2023-04-8T12:00:00", "lat": 40.526101, "lon": -74.039306}, #a: starting point
-            {"time": "2023-04-8T13:00:00", "lat": 39.97322, "lon": -71.954218},
-            
-            ]
+headers = {
+    "spire-api-key": "DaVrozYhAZbbVJVb37AccvH8PjucQXTe",
+    "Content-Type": "application/json",
+}
+payload = json.dumps(
+    {
+        "route": {
+            "name": "my_example_route",
+            "waypoints": [
+                {"time": "2023-04-8T12:00:00", "lat": 40.526101, "lon": -74.039306},
+                {"time": "2023-04-8T13:00:00", "lat": 39.97322, "lon": -71.954218},
+                {"time": "2023-04-8T14:00:00", "lat": 39.485893, "lon": -66.749471},
+                {"time": "2023-04-8T15:00:00", "lat": 37.434774, "lon": -53.753772},
+                {"time": "2023-04-8T16:00:00", "lat": 36.693392, "lon": -42.424287},
+            ],
         },
         "bundles": "basic,maritime",
-}
+    }
+)
+url = "https://api.wx.spire.com/forecast/route"
+response = requests.request("POST", url, headers=headers, data=payload)
 
-hour1 = int(points["route"]["waypoints"][0]["time"][10:12])
-hour2 = int(points["route"]["waypoints"][1]["time"][10:12])
+json_data = response.json()
+print(type(json_data))
+
+
+"""
+hour1 = int(payload["route"]["waypoints"][0]["time"][10:12])
+hour2 = int(payload["route"]["waypoints"][1]["time"][10:12])
+"""
 
 #getting the "lat": 40.526101, "lon": -74.039306 and "lat": 39.97322, "lon": -71.954218
-x_coor1 = float(points["route"]["waypoints"][0]["lat"])
-y_coor1 = float(points["route"]["waypoints"][0]["lon"])
-x_coor2 = float(points["route"]["waypoints"][1]["lat"])
-y_coor2 = float(points["route"]["waypoints"][1]["lon"])
+print(payload)
+x_coor1 = float(json_data["data"][0]["location"]["coordinates"]["lat"])
+y_coor1 = float(json_data["data"][0]["location"]["coordinates"]["lon"])
+x_coor2 = float(json_data["data"][1]["location"]["coordinates"]["lat"])
+y_coor2 = float(json_data["data"][1]["location"]["coordinates"]["lon"])
 
-#slope rise over run
-rise = y_coor2 - y_coor1
-run = x_coor2 - x_coor1
-slope = rise/run
+
+x_mid = (x_coor2 - x_coor1)/2
+y_mid = (y_coor2 - y_coor1)/2
 
 """
 print(x_coor1)
@@ -41,17 +55,22 @@ print(run)
 """
 
 
-while True:
+while True: #keep incrementing coordinates until reached the end
     print(x_coor1)
     print(y_coor1)
-    x_coor1 += slope
-    y_coor1 += slope
-    if x_coor1 >= x_coor2 and run > 0: #break if start is greater than end if run is increases
+    #printing each increment
+    x_coor1 += x_mid
+    y_coor1 += y_mid #incrementing
+    #check the coordinate if it goes above for any of the conditions
+
+    if x_coor1 >= x_coor2 and x_mid > 0: #break if start is greater than end if run is increases
         break
-    elif x_coor1 <= x_coor2 and run < 0: #break if start is less than end if run decreases
+    elif x_coor1 <= x_coor2 and x_mid < 0: #break if start is less than end if run decreases
         break
+
 print(x_coor1)
 print(y_coor1)
+
 
 
     
